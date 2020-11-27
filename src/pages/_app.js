e866d0ead069aa47c44c44c0609479b683617f75
hackwebdev <hackwebdev@gmail.com>
2020-11-27 18:05:34 +0800

    lesson 1

---
+++ src/pages/_app.js
@@ -0,0 +1,13 @@
+import "../../styles/globals.css";
+import Navbar from "../components/navbar";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Component {...pageProps} />
+    </div>
+  );
+}
+
+export default MyApp;
---
+++ src/pages/index.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Index = (props) => {
+  const [joke, setJoke] = useState("");
+
+  useEffect(() => {
+    getJoke();
+  }, []);
+
+  const getJoke = async () => {
+    const res = await axios.get("https://api.chucknorris.io/jokes/random");
+    setJoke(res.data.value);
+  };
+
+  return (
+    <div>
+      <h1>Hello from next js</h1>
+      <p>Description of the page</p>
+      <p>Value CSR: {joke}</p>
+      <p>Value SSR: {props.value}</p>
+    </div>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const res = await axios.get("https://api.chucknorris.io/jokes/random");
+  return {
+    props: {
+      value: res.data.value,
+    },
+  };
+};
+
+export default Index;
